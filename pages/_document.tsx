b623d6548e76862cd0761732e24ed69773f8c92d
Christian Grimsgaard <chrisgrimsgaard@gmail.com>
2021-05-04 10:19:37 +0200

    Added favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,14 +38,13 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="Web site created using create-react-app" />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link rel="manifest" href="/site.webmanifest" />
-          <meta name="msapplication-TileColor" content="#121212" />
-          <meta name="theme-color" content="#121212" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff1e00" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
 
         <body>
